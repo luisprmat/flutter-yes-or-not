a661b4eba3724f400a136b086b685b0a29b4b9e3
Luis Parrado <luisprmat@gmail.com>
2025-04-07 17:01:56 -0500

    Install Provider and setup in main (state management)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yes_or_not_app/config/theme/app_theme.dart';
+import 'package:yes_or_not_app/presentation/providers/chat_provider.dart';
 import 'package:yes_or_not_app/presentation/screens/chat/chat_screen.dart';
 
 void main() {
@@ -11,11 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Yes Or Not App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 1).theme(),
-      home: const ChatScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Yes Or Not App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 1).theme(),
+        home: const ChatScreen(),
+      ),
     );
   }
 }
