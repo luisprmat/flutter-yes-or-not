4b6cc41df9c3cfb2ee7e701245924b94bd6bbcc8
Luis Parrado <luisprmat@gmail.com>
2025-04-07 18:50:13 -0500

    Get and view (in debug) response of YesNo - wtf

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_or_not_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_or_not_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messages = [
     Message(text: 'Hello sweetheart!', fromWho: FromWho.me),
@@ -14,8 +16,14 @@ class ChatProvider extends ChangeNotifier {
     final Message newMessage = Message(text: text, fromWho: FromWho.me);
     messages.add(newMessage);
 
+    if (text.trim().endsWith('?')) herReply();
+
     notifyListeners();
     moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
   }
 
   Future<void> moveScrollToBottom() async {
