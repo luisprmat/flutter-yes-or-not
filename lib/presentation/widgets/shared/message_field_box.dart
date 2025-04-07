0030ff99ec7debb6dfe8ee4129b2010edea9f336
Luis Parrado <luisprmat@gmail.com>
2025-04-07 15:32:36 -0500

    TextFormField for chat messages with options, decorations, etc

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final outlineInputBorder = OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: Icon(Icons.send_outlined),
+        onPressed: () {
+          print('Valor de la caja de texto');
+        },
+      ),
+    );
+
+    return TextFormField(
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit value: $value');
+      },
+      onChanged: (value) {
+        print('Changed: $value');
+      },
+    );
+  }
+}
