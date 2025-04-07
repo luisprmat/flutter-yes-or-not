01f3ed1cd4935c16b1b444619fb8ddd505b52d2c
Luis Parrado <luisprmat@gmail.com>
2025-04-07 16:28:45 -0500

    Create provider for State manager

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:yes_or_not_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messages = [
+    Message(text: 'Hello sweetheart!', fromWho: FromWho.me),
+    Message(text: '¿Ya regresaste del trabajo?', fromWho: FromWho.her),
+  ];
+
+  Future<void> sendMessage(String text) async {
+    // TODO: Implement
+  }
+}
