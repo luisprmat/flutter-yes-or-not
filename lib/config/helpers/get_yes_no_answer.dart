b10784698b171265f0658b7da872a0d2c922bd10
Luis Parrado <luisprmat@gmail.com>
2025-04-07 19:33:09 -0500

    Use QuickType to generate Mappers from json responses

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -10,10 +10,6 @@ class GetYesNoAnswer {
 
     final yesNoModel = YesNoModel.fromJsonMap(response.data);
 
-    return Message(
-      text: yesNoModel.answer,
-      fromWho: FromWho.her,
-      imageUrl: yesNoModel.image,
-    );
+    return yesNoModel.toMessageEntity();
   }
 }
